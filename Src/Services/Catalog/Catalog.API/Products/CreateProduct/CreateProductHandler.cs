58a370e1bde05a44bea66d75fb66f9f941f43f6c
AhmedKhaledSheen <ahmed.khaled@tildetech.ae>
2026-05-08 22:18:27 +0300

    add create Product handle

---
+++ Src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, string Description, List<string> Category, decimal Price, string ImageFile)
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+
+            var product = new Product
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Category = request.Category,
+                Price = request.Price,
+                ImageFile = request.ImageFile
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
