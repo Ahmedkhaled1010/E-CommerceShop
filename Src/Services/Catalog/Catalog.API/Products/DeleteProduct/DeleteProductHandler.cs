89d1d6ee56562edba90fe39ca1dfd7143c8900ab
AhmedKhaledSheen <ahmed.khaled@tildetech.ae>
2026-05-21 02:13:06 +0300

    add-DeleteProduct

---
+++ Src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductHandler
+        (IDocumentSession session, ILogger<DeleteProductHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductHandler.Handle Called with {@Request}", command);
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product == null)
+            {
+                throw new ProductNotFoundException();
+            }
+            session.Delete(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
+        }
+
+    }
+}
