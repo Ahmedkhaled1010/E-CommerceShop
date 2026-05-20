413aad43d129b998cc870385bc6fec239fd24e9b
AhmedKhaledSheen <ahmed.khaled@tildetech.ae>
2026-05-21 02:00:35 +0300

    add-get product by id

---
+++ Src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndPoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Catalog.API.Products.GetProductById
+{
+    // public record GetProductByIdRequest();
+    public record GetProductByIdResponse(Product Product);
+    public class GetProductByIdEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+             {
+                 var result = await sender.Send(new GetProductByIdQuery(id));
+                 var response = result.Adapt<GetProductByIdResponse>();
+                 return Results.Ok(response);
+             })
+             .WithName("GetProductById")
+             .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+             .Produces(StatusCodes.Status404NotFound)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Get a product by id")
+             .WithDescription("Get a product by id")
+             .WithTags("Products");
+
+        }
+    }
+}
